85bc8950edef56b1ec3bffad5d6bcf91ab8134e3
pakuVision <boardguy.vision@gmail.com>
2025-02-09 00:35:52 +0900

    Initial Commit

---
+++ EnergySuckingHand/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  EnergySuckingHand
+//
+//  Created by boardguy.vision on 2025/02/09.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ EnergySuckingHand/EnergySuckingHandApp.swift
@@ -0,0 +1,33 @@
+//
+//  EnergySuckingHandApp.swift
+//  EnergySuckingHand
+//
+//  Created by boardguy.vision on 2025/02/09.
+//
+
+import SwiftUI
+
+@main
+struct EnergySuckingHandApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
