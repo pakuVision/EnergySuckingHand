1c8f8613e91391da8b920e169ecc70f72f44942d
pakuVision <boardguy.vision@gmail.com>
2025-02-13 00:02:40 +0900

    add files

---
+++ Packages/RealityKitContent/Sources/RealityKitContent/OrbComponent.swift
@@ -0,0 +1,11 @@
+import RealityKit
+
+// Ensure you register this component in your app’s delegate using:
+// OrbComponent.registerComponent()
+public struct OrbComponent: Component, Codable {
+    // This is an example of adding a variable to the component.
+    var count: Int = 0
+
+    public init() {
+    }
+}
